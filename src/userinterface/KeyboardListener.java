7efa318f98687b70f9926458363105ee9ab3fefc
Rory <roryskipper123@gmail.com>
2017-05-31 10:20:47 -0600

    Added ctrl listener

---
+++ src/userinterface/KeyboardListener.java
@@ -0,0 +1,29 @@
+package userinterface;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyboardListener implements KeyListener {
+
+	private boolean holdingCtrl = false;
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		this.holdingCtrl = e.isControlDown();
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		this.holdingCtrl = e.isControlDown();
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+
+	}
+
+	public boolean holdingCtrl() {
+		return this.holdingCtrl;
+	}
+
+}
